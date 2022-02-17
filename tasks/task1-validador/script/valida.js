f923cc5621a4cb403fd3a157d47c18adcb85ca57
renaldofreire <renaldo@disroot.org>
2022-02-16 21:32:37 -0300

    fix some files in task1

--- tasks/task1-validador/script/valida.js
+++ tasks/task1-validador/script/valida.js
@@ -3,12 +3,12 @@ function validaLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username=="admin" && password=="admin") {
+    if (username=="admin@mail.com" && password=="admin") {
         alert("Conectado! Você será redirecionado...");
         window.location.replace("home.html");
-    } else {
-        alert("Dados incorretos. Tente novamente.");
-    }
+        return; // else muitas vezes nao necessario
+    } 
+    alert("Dados incorretos. Tente novamente.");
 }
 
 // Mostrar senha
@@ -16,7 +16,10 @@ function verSenha() {
     var temp = document.getElementById("password");
     if (temp.type === "password") {
         temp.type = "text";
-    } else {
-        temp.type = "password";
-    }
+        return;
+    } 
+    temp.type = "password";
 }
+
+
+//dica: return ao inves do else
