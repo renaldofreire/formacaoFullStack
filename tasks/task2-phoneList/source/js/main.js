5ced80e7248dfa1c78454a2faeb894868c2bd5a4
renaldofreire <renaldo@disroot.org>
2022-03-04 04:13:19 -0300

    week7 new updates

--- tasks/task2-phoneList/source/js/main.js
+++ tasks/task2-phoneList/source/js/main.js
@@ -6,19 +6,19 @@ function editRow(no) {
     document.getElementById("editButton"+no).style.display="none";
     document.getElementById("saveButton"+no).style.display="block";
 
-    var name=document.getElementById("nameRow"+no);
-    var phone=document.getElementById("phoneRow"+no);
+    let name=document.getElementById("nameRow"+no);
+    let phone=document.getElementById("phoneRow"+no);
 
-    var nameData=name.innerHTML;
-    var phoneData=phone.innerHTML;
+    let nameData=name.innerHTML;
+    let phoneData=phone.innerHTML;
 
     name.innerHTML="<input type='text' id='nameText"+no+"' value='"+nameData+"'>";
     phone.innerHTML="<input type='text' id='phoneText"+no+"' value='"+phoneData+"'>";
 }
 
 function saveRow(no) {
-    var nameValue=document.getElementById("nameText"+no).value;
-    var phoneValue=document.getElementById("phoneText"+no).value;
+    let nameValue=document.getElementById("nameText"+no).value;
+    let phoneValue=document.getElementById("phoneText"+no).value;
 
     document.getElementById("nameRow"+no).innerHTML=nameValue;
     document.getElementById("phoneRow"+no).innerHTML=phoneValue;
@@ -28,15 +28,13 @@ function saveRow(no) {
 }
 
 function addRow() {
-    var newName=document.getElementById("newName").value;
-    var newPhone=document.getElementById("newPhone").value;
+    let newName=document.getElementById("newName").value;
+    let newPhone=document.getElementById("newPhone").value;
 
-    var table=document.getElementById("dataTable");
-    var tableLen=(table.rows.length)-1;
-    var row = table.insertRow(tableLen).outerHTML="<tr id='row"+tableLen+"'><td id='nameRow"+tableLen+"'>"+newName+"</td><td id='phoneRow"+tableLen+"'>"+newPhone+"</td><td><input type='button' id='editButton"+tableLen+"' value='Editar' class'edit' onclick='editRow("+tableLen+")'> <input type='button' id='saveButton"+tableLen+"' value='Salvar' class='save' onclick='saveRow("+tableLen+")'> <input type='button' value='Deletar' class='delete' onclick='deleteRow("+tableLen+")'></td></tr>";
+    let table=document.getElementById("dataTable");
+    let tableLen=(table.rows.length)-1;
+    let row = table.insertRow(tableLen).outerHTML="<tr id='row"+tableLen+"'><td id='nameRow"+tableLen+"'>"+newName+"</td><td id='phoneRow"+tableLen+"'>"+newPhone+"</td><td><input type='button' id='editButton"+tableLen+"' value='Editar' class'edit' onclick='editRow("+tableLen+")'> <input type='button' id='saveButton"+tableLen+"' value='Salvar' class='save' onclick='saveRow("+tableLen+")'> <input type='button' value='Deletar' class='delete' onclick='deleteRow("+tableLen+")'></td></tr>";
 
     document.getElementById("newName").value="";
     document.getElementById("newPhone").value="";
-
-    console.log(row);
 }
--- week7/fundamentosJS/tiposVariaveisFuncoes/var-let-const.js
+++ week7/fundamentosJS/tiposVariaveisFuncoes/var-let-const.js
@@ -9,16 +9,29 @@
 //var area;
 
 // LET
-let forma = 'retângulo';
-let altura = 5;
-let comprimento = 7;
-let area; //variável vazia
+//let forma = 'retângulo';
+//let altura = 5;
+//let comprimento = 7;
+//let area; //variável vazia
 
-if (forma === 'retângulo') {
+//if (forma === 'retângulo') {
+//area = altura * comprimento;
+//}
+//else {
+//area = (altura * comprimento) / 2;
+//}
+
+//CONST
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
     area = altura * comprimento;
-}
-else {
+} else {
     area = (altura * comprimento) / 2;
 }
 
 console.log(area);
+
