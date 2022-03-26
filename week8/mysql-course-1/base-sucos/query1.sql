ded499a9d23dd4179b77d099f1ad515e6e1d7380
Renaldo Freire <renaldo@disroot.org>
2022-03-26 02:32:54 -0300

    week8 updates

---
+++ week8/mysql-course-1/base-sucos/query1.sql
@@ -0,0 +1,5 @@
+UPDATE tabela_vendedores SET PERCENTUAL_COMISSAO = 0.11
+WHERE MATRICULA = '00236';
+
+UPDATE tabela_vendedores SET NOME = 'José Geraldo da Fonseca Junior'
+WHERE MATRICULA = '00233';
---
+++ week8/mysql-course-1/base-sucos/sql 6.sql
@@ -0,0 +1,7 @@
+INSERT INTO tabela_vendedores (MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES ('00235', 'Márcio Almeida Silva', 0.08);
+
+INSERT INTO tabela_vendedores (MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES ('00236', 'Cláudia Morais', 0.08);
+
+SELECT * FROM tabela_vendedores;
