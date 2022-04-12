16146cc68ca424378af5c87001b3ce59a761faf5
Renaldo Freire <renaldo@disroot.org>
2022-04-12 06:45:18 -0300

    week10 complete

--- week10/nodejs-api-rest/api-petshot/api/banco-de-dados/index.js
+++ week10/nodejs-api-rest/api-petshot/api/banco-de-dados/index.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize')
+const config = require('config')
 
 const instancia = new Sequelize(
-    'petshot',
-    'root',
-    '123456',
+    config.get('mysql.banco-de-dados'),
+    config.get('mysql.usuario'),
+    config.get('mysql.senha'),
     {
-        host: '127.0.0.1'
+        host: config.get('mysql.host'),
         dialect: 'mysql'
     }
-
 )
 
 module.exports = instancia
