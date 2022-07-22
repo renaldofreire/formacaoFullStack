86af5bcf9b29978bf79a5d070c44028e83f9a378
Renaldo Freire <renaldo@disroot.org>
2022-07-22 03:49:24 -0300

    finish html and create css

---
+++ projetos/churrascalc/js/script.js
@@ -0,0 +1,8 @@
+// Sempre definir bem o que o programa irá fazer
+// Regra de negócio
+// Carne - 400g por pessoa + de 6h - 650
+// Cerveja - 1200ml por pessoa + 6h - 2000ml
+// Refrigerante/Água - 1000ml por pessoa + 6h 1500ml
+// Crianças valem por 0,5
+
+
